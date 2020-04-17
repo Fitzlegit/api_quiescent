223156b1394c3889cc58924d861297b32cc45f1c
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2020-04-17 10:40:52 -0400

    added; create, index, show, destroy crud actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,28 @@
 class UsersController < ApplicationController
 
+  def index
+    user = User.all
+    render json: users
+  end
+
+  def create
+    user = User.new(user_params)
+    user.save
+    render json: user
+  end
+
+  def show; end
+
+  def detroy
+    user = User.find_by(id: params[:id])
+    user.delete
+  end
+
+
+  def user_params
+    params.require(:user).permit(:email, :password_digest)
+  end
+
+
 
 end
