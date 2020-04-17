cce565cb47ece94bc99609cb19a33c7c71588bc6
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2020-04-17 10:52:47 -0400

    added CRUD actions

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,2 +1,37 @@
 class ProfilesController < ApplicationController
+  def index
+    profiles = Profile.all
+    render json: profiles
+  end
+
+  def show
+    profile = Profile.find_by(id: params[:id])
+    render json: profile
+  end
+
+  def create
+    newProfile = Profile.new(profile_params)
+    newProfile.save
+    render json: newProfile
+  end
+
+  def update
+    updateProfile = Profile.find_by(:id params[:id])
+    updateProfile.update(profile_params)
+    updateProfile.save
+    render json: updateRecipe
+  end
+
+  def destroy
+    profile = Recipe.find_by(id: params[:id])
+    profile.delete
+  end
+
+  private
+
+  def profile_params
+    params.require(:profile).permit(:name, :why, :meditations, :user_id)
+  end
+
+
 end
